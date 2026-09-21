49bd5061fed0ab80dd6bd36fd7a9e29c1643a1e5
niangdev03 <jamonodev@gmail.com>
2026-09-21 20:14:35 +0000

    error cors

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://komkom.niangdev.com','https://store.niangdev.com'],
+    'allowed_origins' => ['https://komkom.niangdev.com','https://store.niangdev.com','http://164.132.85.222:8081'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
